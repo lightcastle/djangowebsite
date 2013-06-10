97d638ced86951c17cc7073671a040e7875653bb
Josh <josh.s.brown@hotmail.com>
2013-06-10 16:48:56 -0400

    finally got forms working RIGHT. probably not optimal though

--- lightcastle/website/views.py
+++ lightcastle/website/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from django.views.generic import ListView
 from django.views.generic import TemplateView
 from django.template import Context, RequestContext
+from django.http import HttpResponseRedirect
 
 def index(request):
     return HttpResponse("Hello, world. You're at the main site index. \n go to <a href='/login'>admin</a> site")
@@ -40,12 +41,14 @@ def form_valid(self, form):
 def submit(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
-        message=str(form['message'])
-        send_mail('Somebody filled out our contact form', message, 'brownj@lightcastletech.com', ['brownj@lightcastletech.com'], fail_silently=False)
-        print form['name']
-        print form['email']
+        if form.is_valid():
+          message=str(form.cleaned_data['message'])+"\n"+str(form.cleaned_data['email'])
+          send_mail('Somebody filled out our contact form', message, 'brownj@lightcastletech.com', ['brownj@lightcastletech.com'], fail_silently=False)
+          context = Context({'title': 'Contact Us', 'form': form})
+#          print form['name']
+#          print form['email']
 #        form.save
 #        return HttpResponseRedirect('/sendMessage')
-        return render_to_response('sendMessage.html', {'form':form})
+        return render_to_response('sendMessage.html', context, context_instance=RequestContext(request))
 
 
