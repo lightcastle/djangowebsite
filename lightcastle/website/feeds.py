f57ae20fa081f9650af56418100511134b1b93a0
Josh B <josh.s.brown@hotmail.com>
2013-08-02 13:46:00 -0400

    more blogs

--- lightcastle/website/feeds.py
+++ lightcastle/website/feeds.py
@@ -3,8 +3,10 @@ from wordpress_xmlrpc.methods.posts import GetPosts, NewPost
 from wordpress_xmlrpc.methods.users import GetUserInfo
 from django.shortcuts import render_to_response
 from django.template import Template, Context, RequestContext
+
+
 def get_posts(req):
-  wp = Client('http://lightcastletech.wordpress.com/xmlrpc.php', 'brownj@lightcastletech.com', '')
+  wp = Client('http://lightcastletech.wordpress.com/xmlrpc.php', 'brownj@lightcastletech.com', WORDPRESS_PASS)
   all_posts = wp.call(GetPosts({'orderby': 'post_modified', 'number': 100, 'post_status': 'publish'}))
   cont = Context({'title': 'Blog', 'all_posts': all_posts})
   return render_to_response('blog_home.html', cont, context_instance=RequestContext(req))
