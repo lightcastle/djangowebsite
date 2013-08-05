7e8569ab57440ac8c75a0f3b7e0a0e1be0c4310d
Josh B <josh.s.brown@hotmail.com>
2013-08-05 15:07:13 -0400

    re-adding import

--- lightcastle/website/feeds.py
+++ lightcastle/website/feeds.py
@@ -4,13 +4,13 @@ from wordpress_xmlrpc.methods.users import GetUserInfo
 from django.shortcuts import render_to_response
 from django.template import Template, Context, RequestContext
 import os
-
+from django.conf import settings
 
 def get_posts(request):
   wp = Client('http://lightcastletech.wordpress.com/xmlrpc.php', 'brownj@lightcastletech.com', settings.WORDPRESS_PASS)
   all_posts = wp.call(GetPosts({'orderby': 'post_modified', 'number': 100, 'post_status': 'publish'}))
   cont = Context({'title': 'Blog', 'all_posts': all_posts})
-  return render_to_response('blog_home.html', cont, context_instance=RequestContext(request))
+    return render_to_response('blog_home.html', cont, context_instance=RequestContext(request))
 
 def get_specific_post(request, post_id):
   post_id = int(post_id) - 1
