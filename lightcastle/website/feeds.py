f58984d8db76d8b81131918762b41dd72e580974
Josh B <josh.s.brown@hotmail.com>
2013-08-05 14:47:29 -0400

    undoing changes

--- lightcastle/website/feeds.py
+++ lightcastle/website/feeds.py
@@ -4,22 +4,13 @@ from wordpress_xmlrpc.methods.users import GetUserInfo
 from django.shortcuts import render_to_response
 from django.template import Template, Context, RequestContext
 import os
-from django.conf import settings
-from django.core.cache import cache, get_cache
 
 
 def get_posts(request):
-#  try cache.get('blog_posts'):
-#    return cache.get('blog_posts')
-#  except:
-#    pass
-#  else:
-    wp = Client('http://lightcastletech.wordpress.com/xmlrpc.php', 'brownj@lightcastletech.com', settings.WORDPRESS_PASS)
-    all_posts = wp.call(GetPosts({'orderby': 'post_modified', 'number': 100, 'post_status': 'publish'}))
-    cont = Context({'title': 'Blog', 'all_posts': all_posts})
-#    cache.set('blog_posts', render_to_response('blog_home.html', cont, context_instance=RequestContext(request))
-, 10)
-    return render_to_response('blog_home.html', cont, context_instance=RequestContext(request))
+  wp = Client('http://lightcastletech.wordpress.com/xmlrpc.php', 'brownj@lightcastletech.com', settings.WORDPRESS_PASS)
+  all_posts = wp.call(GetPosts({'orderby': 'post_modified', 'number': 100, 'post_status': 'publish'}))
+  cont = Context({'title': 'Blog', 'all_posts': all_posts})
+  return render_to_response('blog_home.html', cont, context_instance=RequestContext(request))
 
 def get_specific_post(request, post_id):
   post_id = int(post_id) - 1
