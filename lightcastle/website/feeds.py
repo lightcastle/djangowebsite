663f2a2d35c975dfbb7541c286fe9cde861205be
Josh B <josh.s.brown@hotmail.com>
2013-08-08 11:31:13 -0400

    regex parsing

--- lightcastle/website/feeds.py
+++ lightcastle/website/feeds.py
@@ -13,7 +13,7 @@ import re
 def get_posts(request):
   wp = Client('http://lightcastletech.wordpress.com/xmlrpc.php', 'brownj@lightcastletech.com', settings.WORDPRESS_PASS)
   all_posts = wp.call(GetPosts({'orderby': 'post_modified', 'number': 100, 'post_status': 'publish'}))
-  _remove_wordpress_markup(all_posts)
+#  parsed_content = _remove_wordpress_markup(all_posts)
 
   current_time = datetime.datetime.now()
   cont = Context({'title': 'Blog', 'all_posts': parsed_content, 'current_time': current_time})
@@ -24,15 +24,20 @@ def get_specific_post(request, post_id):
   wp = Client('http://lightcastletech.wordpress.com/xmlrpc.php', 'brownj@lightcastletech.com', settings.WORDPRESS_PASS)
   blog_post = wp.call(GetPosts({'orderby': 'post_modified', 'number': 100, 'post_status': 'publish'}))
   blog_post = blog_post[post_id]
-  _remove_wordpress_markup(blog_post)
+  parsed_content = _remove_wordpress_markup(blog_post)
   context = Context({'title': 'Blog', 'blog_post': parsed_content})
   return render_to_response('blog_post.html', context, context_instance=RequestContext(request))
 
 
+
+
+
 def _remove_wordpress_markup(source):
-  substitute = source.content.replace(u'\xa0',u'')
-  language = re.search(r'\[sourcecode language=\"(.*)\"\]', substitute, re.U)
-  parsed_content = re.sub(r'\[sourcecode language=.*\]', '<code class="+str(language.group())+">', source)
+  substitute = source.content.replace(u'\xa0', u'')
+  regex = re.compile(r'\[sourcecode language=\"(.*)\"\]')
+  parsed_content = regex.sub(r'<code class="+str(language.group())">', blog_post.content)
+#  parsed_content = re.sub(r'\[sourcecode language=(.*)\]', '<code class="+str(language.group())">', source)
+#  parsed_content = re.sub(r'\[/sourcecode\]', '</code>', source)
 #  remove [caption]
   return parsed_content
   
