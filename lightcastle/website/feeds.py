19640a7be1dc175b47153919826ea124ec4b986b
Josh B <josh.s.brown@hotmail.com>
2013-08-22 13:01:14 -0400

    changing blog header for subpages

--- lightcastle/website/feeds.py
+++ lightcastle/website/feeds.py
@@ -28,7 +28,6 @@ def get_posts(request):
   cont = Context({'title': 'Blog', 'all_posts': all_posts, 'current_time': current_time})
   return render_to_response('blog_home.html', cont, context_instance=RequestContext(request))
 
-
 def get_specific_post(request, post_id):
   post_id = int(post_id) - 1
   wp = Client('http://lightcastletech.wordpress.com/xmlrpc.php', 'brownj@lightcastletech.com', settings.WORDPRESS_PASS)
@@ -71,11 +70,7 @@ def get_latest_blog(request):
   cont = Context({'title': 'Blog', 'latest_post': latest_post})
   return render_to_response('index.html', cont, context_instance=RequestContext(request))
 
-
-
 def _remove_wordpress_markup(source):
-
-
   pattern_one = re.compile(r'\[sourcecode language="(.*)"\]')
   pattern_two = re.compile(r'\[caption.*\]')
   pattern_three = re.compile(r'\[/sourcecode\]')
@@ -85,17 +80,14 @@ def _remove_wordpress_markup(source):
     match = re.search('\[sourcecode language="(.*)"\]', source)
     match = match.group(1).lower()
   else:
-    match = ""
+    match = "" # set an empty variable here because you'll get errors on the blog index page if there is no sourcecode language found
     
-
   parsed_content = pattern_one.sub(r'<pre class="brush:'+match+'">', source)
   parsed_content = pattern_two.sub(r'<caption>', parsed_content)
   parsed_content = pattern_three.sub(r'</pre>', parsed_content)
   parsed_content = pattern_four.sub(r'</caption>', parsed_content)
   return parsed_content
   
-
-
 def _remove_html_tags(source):
   pattern_three = re.compile(r'<.*?>')
   parsed_content = pattern_three.sub(r'', source)
