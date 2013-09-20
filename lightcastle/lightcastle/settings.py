7a05b1b82ee6ebcf3ab1895291163eb8028aa0e8
Josh B <josh.s.brown@hotmail.com>
2013-09-19 21:55:23 -0400

    changing settings

--- lightcastle/lightcastle/settings.py
+++ lightcastle/lightcastle/settings.py
@@ -128,7 +128,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.markup',
-    'jquery',
+#    'jquery',
 #    'wordpress',
     # Uncomment the next line to enable the admin:
 #    'django.contrib.admin',
