3af798ac27d9487e89e89019feef999302346651
Josh B <josh.s.brown@hotmail.com>
2013-10-09 13:59:08 -0400

    cleaning up code some

--- lightcastle/website/blog.py
+++ lightcastle/website/blog.py
@@ -5,19 +5,27 @@ from wordpress_xmlrpc.methods.posts import GetPosts, NewPost, GetPost
 from wordpress_xmlrpc.methods.users import GetUserInfo, GetAuthors
 from bs4 import BeautifulSoup
 
+
+def _set_markdown_language(source):
+  #FIXME: need to set this so that it will register if there are multiple sourcecode languages in a blog. currently doesn't do that. Example of this would be in "Learning my way around gspread" blog.
+  # set an empty variable here because you'll get errors on the blog index page if the variable isn't set
+  language = ""
+
+  ### this if/else group is to set match variable  so that it doesnt screw up the blog_index page, which also uses this logic
+  if re.search('\[sourcecode language="(.*)"\]', source): 
+    language = re.search('\[sourcecode language="(.*)"\]', source)
+    #the following line catches any language names that are camel cased and lowercases them since our code highlighting parser needs all lower case
+    language = language.group(1).lower() 
+  return language
+
 def _remove_wordpress_markup(source):
+  #this method changes all the markup from wordpress into the right format for our syntax highlighter to recognize it.
   pattern_one = re.compile(r'\[sourcecode language="(.*)"\]|\[sourcecode\]')
   pattern_two = re.compile(r'\[caption.*\]')
   pattern_three = re.compile(r'\[/sourcecode\]')
   pattern_four = re.compile(r'\[/caption\]')
 
-  if re.search('\[sourcecode language="(.*)"\]', source): ### this if/else group is to set match variable  so that it doesnt screw up the blog_index page, which also calls this function. could break this out into a separate method. TODO clean this up
-    match = re.search('\[sourcecode language="(.*)"\]', source)
-    match = match.group(1).lower() #this catches any language names that are camel cased and lowercases them since our code highlighting parser needs all lower case
-  else:
-    match = "" # set an empty variable here because you'll get errors on the blog index page if there is no sourcecode language found
-    
-  parsed_content = pattern_one.sub(r'<pre class="brush:'+match+'">', source)
+  parsed_content = pattern_one.sub(r'<pre class="brush:'+_set_markdown_language(source)+'">', source)
   parsed_content = pattern_two.sub(r'<caption>', parsed_content)
   parsed_content = pattern_three.sub(r'</pre>', parsed_content)
   parsed_content = pattern_four.sub(r'</caption>', parsed_content)
@@ -33,17 +41,19 @@ def _get_first_image(source):
   return str(soup.img)
 
 
+
 class Blog():
   def __init__(self):
     self.wp = Client('http://lightcastletech.wordpress.com/xmlrpc.php', 'brownj@lightcastletech.com', settings.WORDPRESS_PASS)
     self.all_posts = self.wp.call(GetPosts({'orderby': 'post_modified', 'number': 100, 'post_status': 'publish'}))
     self.authors = self.wp.call(GetAuthors())
-    self.all_posts = self.all_posts[::-1]
+    self.all_posts = self.all_posts[::-1] #reverse the list of posts so that the most recent are last in the list
     for post in self.all_posts: #this and the nested for-loop sets the author's display name because it isnt handled already by xmlrpc library
       for author in self.authors:
         if author.id == post.user:
           post.author = author.display_name
-      if _get_first_image(post.content) != "None": #sets the image variable so the posts index can display right
+      #following line sets the image variable so the posts index can display right
+      if _get_first_image(post.content) != "None": 
         post.image = _get_first_image(post.content)
 
 
