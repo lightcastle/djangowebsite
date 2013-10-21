49f7f7c9c34a9fb2c4b701a21641945710c41351
Josh B <josh.s.brown@hotmail.com>
2013-10-21 15:55:25 -0400

    model updates and test updates

--- lightcastle/website/management/commands/update_database.py
+++ lightcastle/website/management/commands/update_database.py
@@ -1,3 +1,4 @@
+from optparse import make_option
 from django.core.management.base import BaseCommand, CommandError
 from django.db import models
 from django.conf import settings
@@ -9,26 +10,49 @@ from website.models import blog
 
 class Command(BaseCommand):
     help = "updates the database of blog entries with the most current information"
+    option_list = BaseCommand.option_list + (
+        make_option("--find-new-post",
+            action='store_true',
+            dest='find-new-post',
+            default=False,
+            help='Delete poll instead of closing it'),
+        )
+
+    def __init__(self, *args, **kwargs):
+        super(Command, self).__init__(*args, **kwargs)
+        wp = Client('http://lightcastletech.wordpress.com/xmlrpc.php', 'brownj@lightcastletech.com', settings.WORDPRESS_PASS)
+        self.all_posts = wp.call(GetPosts({'number': 100, 'post_status': 'publish'}))
+        self.authors = wp.call(GetAuthors())
+        self.all_posts = self.all_posts[::-1] #reverse the list of posts so that the most recent are last in the list
 
 
     def handle(self, *args, **options):
-        wp = Client('http://lightcastletech.wordpress.com/xmlrpc.php', 'brownj@lightcastletech.com', settings.WORDPRESS_PASS)
-        all_posts = wp.call(GetPosts({'number': 100, 'post_status': 'publish'}))
-        authors = wp.call(GetAuthors())
-        all_posts = all_posts[::-1] #reverse the list of posts so that the most recent are last in the list
 
-        for post in all_posts: #this and the nested for-loop sets the author's display name because it isnt handled already by xmlrpc library
-            for author in authors:
-                if author.id == post.user:
-                    post.author = author.display_name
-#       following line sets the image variable so the posts index can display right
-            if blog._get_first_image(post.content) != "None":
-                post.image = blog._get_first_image(post.content)
-            else:
-              post.image = ""
+        if options['find-new-post']:
+            pass
+            # potential code to update database
+            # if len(all_posts) == len(b)+1:
+            #   new_post = blog.Blog(title=all_posts[-1].title, author=all_posts[-1].author,content=all_posts[-1].content,initial_image=all_posts[-1].image, date = all_posts[-1].date)
+
+        else:
+#          this and the nested for-loop sets the author's display name because it isnt handled already by xmlrpc library
+            for post in self.all_posts: 
+                for author in self.authors:
+                    if author.id == post.user:
+                        post.author = author.display_name
+#              following line sets the image variable so the posts index can display right
+                if blog._get_first_image(post.content) != "None":
+                    post.image = blog._get_first_image(post.content)
+                else:
+                  post.image = ""
+
+                b = blog.Blog( title = post.title, author = post.author, initial_image = post.image, date = post.date, content = post.content)
+                b.save()
+#               self.stdout.write('wrote %r to the database\n' % b.title)
 
 
-            b = blog.Blog( title = post.title, author = post.author, initial_image = post.image, date = post.date, content = post.content)
-            b.save()
-#            self.stdout.write('wrote %r to the database\n' % b.title)
+
+
+
+
 
--- lightcastle/website/tests/test_models.py
+++ lightcastle/website/tests/test_models.py
@@ -1,15 +1,38 @@
+import re
+import os
 from django.utils import unittest
 from website.models import blog
 from website.management.commands.update_database import Command
-import re
-
+from django.conf import settings
+from wordpress_xmlrpc import Client, WordPressPost
+from wordpress_xmlrpc.methods.posts import GetPosts, NewPost, GetPost
+from wordpress_xmlrpc.methods.users import GetUserInfo, GetAuthors
 
 
 class BlogTestCase(unittest.TestCase):
     def setUp(self):
+        wp = Client('http://lightcastletech.wordpress.com/xmlrpc.php', 'brownj@lightcastletech.com', settings.WORDPRESS_PASS)
+        self.all_posts = wp.call(GetPosts({'number': 100, 'post_status': 'publish'}))
+        self.authors = wp.call(GetAuthors())
+        self.all_posts = self.all_posts[::-1] #reverse the list of posts so that the most recent are last in the list
+        for post in self.all_posts: 
+            for author in self.authors:
+                if author.id == post.user:
+                    post.author = author.display_name
+#          following line sets the image variable so the posts index can display right
+            if blog._get_first_image(post.content) != "None":
+                post.image = blog._get_first_image(post.content)
+            else:
+              post.image = ""
+
+            b = blog.Blog( title = post.title, author = post.author, initial_image = post.image, date = post.date, content = post.content)
+            b.save()
+
+
+
 #        self.b = Blog()
-        c = Command()
-        c.handle()
+#        os.system("pwd")#python manage.py update_database")#c = Command()
+#        c.handle()
 
 
 
